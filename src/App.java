7dd71b3a4cb61f1011904495402745ecca10d733
Lafa <lafalasidosi@pm.me>
2020-08-17 16:14:09 -0700

    create tables and insert

--- src/App.java
+++ src/App.java
@@ -1,29 +1,48 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 import java.sql.SQLException;
 
 public class App {
-    private final String url = "jdbc:postgresql://localhost/mydb";
-    private final String user = "john";
-    private final String password = "h74AX9ws";
-
-
-    public Connection connect() {
-
-
-        Connection conn = null;
+    public static void main(String[] args) {
+        final String url = "jdbc:postgresql://localhost/mydb";
+        final String user = "john";
+        final String password = "h74AX9ws";
+        Connection c = null;
+        Statement stmt = null;
         try {
-            conn = DriverManager.getConnection(url, user, password);
+            c = DriverManager.getConnection(url, user, password);
             System.out.println("Connected to the PostgreSQL server successfully.");
+            stmt = c.createStatement();
+//            String sql = "CREATE TABLE COMPANY " +
+//                    "(ID INT PRIMARY KEY     NOT NULL," +
+//                    " NAME           TEXT    NOT NULL, " +
+//                    " AGE            INT     NOT NULL, " +
+//                    " ADDRESS        CHAR(50), " +
+//                    " SALARY         REAL)";
+//            stmnt.executeUpdate(sql);
+
+            String sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+                    + "VALUES (1, 'Paul', 32, 'California', 20000.00 );";
+            stmt.executeUpdate(sql);
+
+            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+                    + "VALUES (2, 'Allen', 25, 'Texas', 15000.00 );";
+            stmt.executeUpdate(sql);
+
+            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+                    + "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );";
+            stmt.executeUpdate(sql);
+
+            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+                    + "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
+            stmt.executeUpdate(sql);
+
+            stmt.close();
+            c.close();
+
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-
-        return conn;
-    }
-
-    public static void main(String[] args) {
-        App app = new App();
-        app.connect();
     }
 }
