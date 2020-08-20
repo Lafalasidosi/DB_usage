3eaf4e6495c56d04fb0890b371ad2c4713ad9901
Lafa <lafalasidosi@pm.me>
2020-08-19 22:38:12 -0700

    reads from db to stdout

--- src/App.java
+++ src/App.java
@@ -1,45 +1,80 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+// import javafx.scene.control.CheckBox;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.geometry.*;
+import javafx.collections.ObservableList;
+import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.Statement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.security.SecureRandom;
 
-public class App {
+public class App extends Application {
+
+    Stage window;
+    Scene scene;
+    Button button;
+    ListView<String> listView;
+
     public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        /// database connection
         final String url = "jdbc:postgresql://localhost/mydb";
         final String user = "john";
         final String password = "h74AX9ws";
-        Connection c = null;
-        Statement stmt = null;
-        String depts[] = {"Human Resources", "IT", "Sales", "Warehouse", "Management", "Financial"};
-        String firstNames[] = {"Mark", "Joan", "Kelly", "Henry", "Dagny", "Eugene"};
-        String lastNames[] = {"Hamilton", "Spencer", "Gerk", "Rearden", "Taggart", "Levy"};
-        SecureRandom rand = new SecureRandom();
 
-        try {
-            c = DriverManager.getConnection(url, user, password);
-            System.out.println("Connected to the PostgreSQL server successfully.");
-            stmt = c.createStatement();
 
-            String sql = "CREATE TABLE COMPANY " +
-                    "(ID    INT," +
-                    " NAME           VARCHAR(20), " +
-                    " DEPT            VARCHAR(50), " +
-                    " EID              INT, " +
-                    " SALARY         INT)";
-            stmt.executeUpdate(sql);
+        try(Connection c = DriverManager.getConnection(url, user, password);
+            PreparedStatement stmt = c.prepareStatement("Select * from company");
+            ResultSet rs = stmt.executeQuery()) {
 
-            for (int i = 0; i < 1000; i++) {
-                sql = String.format("INSERT INTO COMPANY (ID, NAME, DEPT, EID, SALARY) VALUES (%d, '%s %s', '%s', %d, %d);", i+1, firstNames[rand.nextInt(6)],
-                                        lastNames[rand.nextInt(6)], depts[rand.nextInt(6)], i, (2000 + rand.nextInt(5000)));
-                stmt.executeUpdate(sql);
+
+            while(rs.next()) {
+                for(int i = 1; i < 6; i++) {
+                    System.out.print(rs.getString(i) + " ");
+                }
+                System.out.println();
             }
 
-            stmt.close();
-            c.close();
-
-        } catch (SQLException e) {
+        } catch(SQLException e){
             System.out.println(e.getMessage());
         }
+
+        window = primaryStage;
+        window.setTitle("lafa's list project");
+        button = new Button("Submit");
+
+        listView = new ListView<>();
+        listView.getItems().addAll("Titanic", "Chicken Run", "Ender's Game", "Pulp Fiction", "Kill Bill",
+                "The Emperor's New Groove", "Melancholia");
+        listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
+        button.setOnAction(e -> buttonClicked());
+
+        // Layout
+        VBox layout = new VBox(10);
+        layout.setPadding(new Insets(20, 20, 20, 20));
+        layout.getChildren().addAll(listView, button);
+
+        // construct window
+        scene = new Scene(layout, 1500, 1250);
+        window.setScene(scene);
+        window.show();
+
     }
+
+    private void buttonClicked() {
+        listView.getSelectionModel().getSelectedItems().stream().forEach(s -> System.out.println(s));
+        System.out.println();
+    }
+
 }
